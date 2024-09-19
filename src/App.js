712b9c3161350b90c359c143a4316c932f4b49df
Bye-B <eliassuter@bluewin.ch>
2024-09-19 21:59:56 +0200

    some updates in register

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RegisterForm from './register';
 
 function App() {
+  const goToLogin = () => {
+    alert('Navigating to Login Page...');
+    // Logic to navigate to login page can be added here, e.g.:
+    // navigate('/login');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RegisterForm onLogin={goToLogin} />
     </div>
   );
 }
